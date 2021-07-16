ba5db3a3c62d221d10c4aaf9b623b154e7c1ad3e
Lucas Felipe da Silva Paiva <lucasfelipespaiva@gmail.com>
2021-07-16 16:00:39 -0300

    Projeto versao 1.0

--- Projeto.py
+++ Projeto.py
@@ -152,28 +152,32 @@ class Moto(Veiculo):
 
 
 # ----------------------------------
-pil1 = Piloto('Jose', 21, 1)
-pil2 = Piloto('Joaquim', 35, 1)
-pil3 = Piloto('Henrique', 32, 1)
+pil1 = Piloto('Jose', 21, 0.87)
+pil2 = Piloto('Joaquim', 35, 0.92)
+pil3 = Piloto('Henrique', 32, 0.95)
 carros = []
-carros.append(Carro(0, 0, 'Porsche', '1954', 10, 55, pil1, 'Hidráulica'))
+carros.append(Carro(0, 0, 'Porsche', '1954', 10, 50, pil1, 'Hidráulica'))
 carros.append(Carro(0, 0, 'McLaren', '2001', 10, 50, pil2, 'Hidráulica'))
-carros.append(Carro(0, 0, 'Mustang', '1900', 10, 50, pil3, 'Hidráulica'))
+carros.append(Carro(0, 0, 'Mustang', '1900', 10, 56, pil3, 'Hidráulica'))
 # -----------------------------------
 
 carroVencedor = ''
 menor_Tempo = 0
-contador = 0
+
 for carroAtual in carros:
     distPercorrida = 0
     contador = 0
+
     while distPercorrida < 1000:
         contador += 1
         carroAtual.acelerar()
         distPercorrida += carroAtual.veloc
+
     if menor_Tempo == 0:
         menor_Tempo = contador
-    elif contador < menor_Tempo:
+        carroVencedor = f'{carroAtual.marca} {carroAtual.modelo}'
+
+    if contador < menor_Tempo:
         menor_Tempo = contador
         carroVencedor = f'{carroAtual.marca} {carroAtual.modelo}'
 
---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Corrida
+
+Uma empresa decidiu fazer um simulador de corridas para testar a capacidade de seus projetos de veículos.
+Cada veículo possui como características: marca, modelo, potência do motor, peso e velocidade máxima, também possuem uma aceleração característica e estão divididos em duas categorias (Carros e Motos).
+Nessa empresa os pilotos de teste são classificados por sua experiência. Com base nesses dados o simulador vai, através de uma fórmula, fazer uma estimativa de tempo em que cada veículo irá atingir a distância definida e escolher aquele o qual fez o percurso no menor intervalo de tempo.
