01c5cb1b2a87f0c7a9402f235925a716d198cbca
Sergio Mendonca <sftdom@gmail.com>
2021-07-18 00:01:36 -0300

    cadastrocarro and cadastromoto updated.

--- cadastrocarro.py
+++ cadastrocarro.py
@@ -1,5 +1,6 @@
 from carro import Carro
 from piloto import Piloto
+from cadastropiloto import CadastroPiloto
 
 carros = []
 
@@ -9,20 +10,71 @@ class CadastroCarro(Carro):
         super().__init__(on, vel, marca, modelo, aceleracao, velocMax, pil, tpDirecao)
 
     def incluir():
-        pass
+        marca = input('Marca: ')
+        modelo = input('Modelo: ')
+        on = bool(input('Ligado/Desligado: '))
+        vel = float(input('Velocidade: '))
+        aceleracao = float(input('Aceleração: '))
+        velocMax = float(input('Velocidade Máxima: '))
+        tpDirecao = input('Tipo de Direção: ')
+        pil = CadastroPiloto.incluir()
+        c = CadastroCarro(on, vel, marca, modelo, aceleracao,
+                          velocMax, pil, tpDirecao)
+        carros.append(c)
+        return c
 
-    def remover():
-        pass
+    def remover(marca):
+        status = False
+        if len(carros) == 0:
+            print('Não há registros em Carros.')
+        else:
+            for idx, carro in enumerate(carros):
+                if str(marca) == str(carro.marca):
+                    del(carros[idx])
+                    print(f'Carro {marca} removido.')
+                    status = True
+                else:
+                    print(f'Carro {marca} não encontrado.')
+        return status
 
-    def consultar():
-        pass
+    def consultar(marca):
+        if len(carros) == 0:
+            print('Não há registros em Carros.')
+        else:
+            for carro in carros:
+                if str(marca) == str(carro.marca):
+                    print(f'Carro {marca} se encontra nos registros.')
+                    return carro
+                else:
+                    print(f'Carro {marca} não consta nos registros.')
 
-    def atualizar():
-        pass
+    def atualizar(marca):
+        if len(carros) == 0:
+            print('Não há registros em Carros.')
+        else:
+            for idx, carro in enumerate(carros):
+                if str(marca) == str(carro.marca):
+                    print(
+                        f'Carro {marca} encontrado em nossos registros.\nVamos atualizar os dados: ')
+                    marca = input('Marca: ')
+                    modelo = input('Modelo: ')
+                    on = bool(input('Ligado/Desligado: '))
+                    vel = float(input('Velocidade: '))
+                    aceleracao = float(input('Aceleração: '))
+                    velocMax = float(input('Velocidade Máxima: '))
+                    tpDirecao = input('Tipo de Direção: ')
+                    pil = CadastroPiloto.incluir()
+                    c_aux = CadastroCarro(on, vel, marca, modelo, aceleracao,
+                                          velocMax, pil, tpDirecao)
+                    carros[idx] = c_aux
+                    print('Carro atualizado.')
+            else:
+                print(f'Carro {marca} não consta nos registros.')
 
 
 def main():
-    pass
+    carro1 = CadastroCarro.incluir()
+    print(carro1.__dict__)
 
 
 if __name__ == '__main__':
--- cadastromoto.py
+++ cadastromoto.py
@@ -1,5 +1,6 @@
 from moto import Moto
 from piloto import Piloto
+from cadastropiloto import CadastroPiloto
 
 motos = []
 
@@ -9,16 +10,66 @@ class CadastroMoto(Moto):
         super().__init__(on, veloc, marca, modelo, aceleracao, peso, velocMax, pil, tpGuidao)
 
     def incluir():
-        pass
+        marca = input('Marca: ')
+        modelo = input('Modelo: ')
+        on = bool(input('Ligado/Desligado: '))
+        vel = float(input('Velocidade: '))
+        aceleracao = float(input('Aceleração: '))
+        velocMax = float(input('Velocidade Máxima: '))
+        tpGuidao = input('Tipo de Direção: ')
+        pil = CadastroPiloto.incluir()
+        m = CadastroMoto(on, vel, marca, modelo, aceleracao,
+                         velocMax, pil, tpGuidao)
+        motos.append(m)
+        return m
 
-    def remover(nome):
-        pass
+    def remover(marca):
+        status = False
+        if len(motos) == 0:
+            print('Não há registros em Motos.')
+        else:
+            for idx, moto in enumerate(motos):
+                if str(marca) == str(moto.marca):
+                    del(motos[idx])
+                    print(f'Moto {marca} removida.')
+                    status = True
+                else:
+                    print(f'Moto {marca} não encontrada.')
+        return status
 
-    def consultar(nome):
-        pass
+    def consultar(marca):
+        if len(motos) == 0:
+            print('Não há registros em Motos.')
+        else:
+            for moto in motos:
+                if str(marca) == str(moto.marca):
+                    print(f'Moto {marca} se encontra nos registros.')
+                    return moto
+                else:
+                    print(f'Moto {marca} não consta nos registros.')
 
-    def atualizar(nome):
-        pass
+    def atualizar(marca):
+        if len(motos) == 0:
+            print('Não há registros em motos.')
+        else:
+            for idx, moto in enumerate(motos):
+                if str(marca) == str(moto.marca):
+                    print(
+                        f'Moto {marca} encontrado em nossos registros.\nVamos atualizar os dados: ')
+                    marca = input('Marca: ')
+                    modelo = input('Modelo: ')
+                    on = bool(input('Ligado/Desligado: '))
+                    vel = float(input('Velocidade: '))
+                    aceleracao = float(input('Aceleração: '))
+                    velocMax = float(input('Velocidade Máxima: '))
+                    tpDirecao = input('Tipo de Direção: ')
+                    pil = CadastroPiloto.incluir()
+                    m_aux = CadastroMoto(on, vel, marca, modelo, aceleracao,
+                                         velocMax, pil, tpDirecao)
+                    motos[idx] = m_aux
+                    print('Moto atualizado.')
+            else:
+                print(f'Moto {marca} não consta nos registros.')
 
 
 def main():
